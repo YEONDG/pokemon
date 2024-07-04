3090ddd8da6de19a566dbf865f9debcb9ab6318f
YDG <zzmn1234@naver.com>
2024-07-04 13:47:11 +0900

    chore: rollup-plugin-visualizer 설치

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
 import path from "path";
 import react from "@vitejs/plugin-react";
+import { visualizer } from "rollup-plugin-visualizer";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    visualizer({
+      filename: "./dist/stats.html",
+      open: true,
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
