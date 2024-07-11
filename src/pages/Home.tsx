0e61fbfaa2226df1db2b9c22caef7c8bb2eacc38
YDG <zzmn1234@naver.com>
2024-07-11 13:23:22 +0900

    refactor: maintypeslabel suspense 적용

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,11 @@
 import ErrorBoundaryWrapper from "@/components/error-boundary-wrapper";
+import SearchBar from "@/components/search/search-bar";
 import { PokemonListSkeleton } from "@/components/skeleton/pokemon-list-skeleton";
 import { lazy, Suspense } from "react";
 import { Helmet } from "react-helmet-async";
 
 const PokemonList = lazy(() => import("@/components/pokemon-list"));
 const MainTypesLabel = lazy(() => import("@/components/main-types-label"));
-const SearchBar = lazy(() => import("@/components/search/search-bar"));
 
 const HomePage = () => {
   return (
@@ -15,7 +15,11 @@ const HomePage = () => {
       </Helmet>
       <div className="flex h-full flex-col items-center justify-center">
         {/* 타입리스트 */}
-        <MainTypesLabel />
+        <ErrorBoundaryWrapper>
+          <Suspense fallback={<div>Loading Types...</div>}>
+            <MainTypesLabel />
+          </Suspense>
+        </ErrorBoundaryWrapper>
         {/* 서치바 */}
         <SearchBar />
         {/* 리스트 */}
