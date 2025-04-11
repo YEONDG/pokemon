e6b37f33a7d856faffaa699ed24f79c4f7ca2f88
YDG <zzmn1234@naver.com>
2025-04-11 23:17:47 +0900

    refactor: nav 시멘틱태그 리팩토링

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -4,19 +4,17 @@ import { ModeToggle } from "./mode-toggle";
 
 export const NavBar = () => {
   return (
-    <div className="fixed top-0 z-10 h-20 w-full">
+    <nav className="fixed top-0 z-10 h-20 w-full">
       <div className="container mx-auto">
-        <div className="flex justify-between px-6">
-          <Link to={"/"} className="m-5 flex text-3xl dark:text-slate-100">
+        <div className="flex items-center justify-between px-6">
+          <Link to="/" className="m-5 flex text-3xl dark:text-slate-100">
             <img src="/poke.svg" width={30} height={30} alt="포켓몬 아이콘" />
-            <span className="hidden md:block">포켓몬도감</span>
+            <h1 className="hidden md:block">포켓몬도감</h1>
           </Link>
 
-          <div className="m-5 text-3xl">
-            <ModeToggle />
-          </div>
+          <ModeToggle />
         </div>
       </div>
-    </div>
+    </nav>
   );
 };
