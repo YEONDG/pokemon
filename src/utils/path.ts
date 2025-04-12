baa6767e26ae5dce48871434c6575d422664bb5b
YDG <zzmn1234@naver.com>
2025-04-12 13:17:14 +0900

    refactor: 포켓몬 이미지 경로 리팩토링

--- src/utils/path.ts
+++ src/utils/path.ts
@@ -1,10 +1,18 @@
 import { PokemonDetailType } from "../types";
 
 export const pokemonImgSrc = (pokemonInfo: PokemonDetailType) => {
-  return (
+  // 애니메이션 이미지 (1순위)
+  const animatedImage =
     pokemonInfo?.sprites?.versions?.["generation-v"]?.["black-white"]?.animated
-      ?.front_default ??
-    pokemonInfo?.sprites?.front_default ??
-    pokemonInfo?.sprites?.other?.["official-artwork"].front_default
-  );
+      ?.front_default;
+
+  // 기본 이미지 (2순위)
+  const defaultImage = pokemonInfo?.sprites?.front_default;
+
+  // 공식 아트워크 이미지 (3순위)
+  const officialArtwork =
+    pokemonInfo?.sprites?.other?.["official-artwork"].front_default;
+
+  // 우선순위대로 이미지 반환
+  return animatedImage || defaultImage || officialArtwork;
 };
