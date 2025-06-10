12e38a0c3f676d482b9676c893b0baf453742096
YDG <zzmn1234@naver.com>
2025-06-10 21:54:23 +0900

    style: update TypeItem component for improved layout and consistency

--- src/components/home/type-nav/type-item.tsx
+++ src/components/home/type-nav/type-item.tsx
@@ -11,11 +11,11 @@ export const TypeItem = memo(({ type }: TypeItemProps) => {
   return (
     <Link
       to={`pokemon/type/${type}`}
-      className={` ${typeBgColor[type]} w-25 flex h-16 items-center justify-center rounded-lg px-3 text-center font-bold text-white shadow-md transition-all duration-200 hover:scale-105 hover:shadow-lg sm:rounded-xl md:h-14 md:w-28`}
+      className={` ${typeBgColor[type]} flex items-center justify-center rounded-lg px-2 py-1 text-center font-bold text-white shadow-md transition-all duration-200 hover:scale-105 hover:shadow-lg sm:rounded-xl md:h-14 md:w-28 md:px-3 md:py-4`}
     >
       <div className="flex flex-col items-center">
         <span className="text-xs opacity-80">{type.toUpperCase()}</span>
-        <span className="mt-1 text-lg">{typeConverter[type]}</span>
+        <span className="text-xs md:text-lg">{typeConverter[type]}</span>
       </div>
     </Link>
   );
