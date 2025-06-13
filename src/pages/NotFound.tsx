921740895c74aed1a802111bf22f0b0cc1e6b95e
YDG <zzmn1234@naver.com>
2025-06-13 19:22:41 +0900

    style: add dark mode text color to NotFoundPage for improved accessibility

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 const NotFoundPage = () => {
   return (
-    <div className="flex h-screen flex-col items-center justify-center gap-4">
+    <div className="flex h-screen flex-col items-center justify-center gap-4 dark:text-white">
       <h1>404 - 페이지를 찾을 수 없습니다</h1>
       <p>요청하신 페이지가 존재하지 않거나, 주소가 잘못되었습니다.</p>
       <Link
